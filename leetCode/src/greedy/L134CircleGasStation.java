8cbda8b5c64128c32b14c36457946ff6d3cb7dc4
huangcheng04 <huangcheng04@meituan.com>
2020-11-19 00:43:15 +0800

    feat(leetcode): l134-greedy-GasStationInCircle-2020-11-18

--- leetCode/src/greedy/L134CircleGasStation.java
+++ leetCode/src/greedy/L134CircleGasStation.java
@@ -30,7 +30,9 @@ public class L134CircleGasStation {
         L134CircleGasStation m = new L134CircleGasStation();
         int[] gas = {1,2,3,4,5};
         int[] cost = {3,4,5,1,2};
-        m.canCompleteCircuit(gas, cost);
+        int a1 = m.canCompleteCircuit(gas, cost);
+        int a2 = m.canCompleteCircuit2(gas, cost);
+        System.out.println(a1 + "\n" + a2);
     }
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
@@ -59,5 +61,20 @@ public class L134CircleGasStation {
             costSum = 0;
         }
         return -1;
+    }
+
+    // https://leetcode-cn.com/problems/gas-station/solution/shi-yong-tu-de-si-xiang-fen-xi-gai-wen-ti-by-cyayc/
+    public int canCompleteCircuit2(int[] gas, int[] cost) {
+        int n = gas.length;
+        int spare = 0, minSpare = Integer.MAX_VALUE;
+        int ans = -1;
+        for(int i = 0; i < n; i++){
+            spare += gas[i] - cost[i];
+            if(minSpare > spare){
+                minSpare = spare;
+                ans = i;
+            }
+        }
+        return spare >= 0 ? (ans + 1) % n: -1;
     }
 }
