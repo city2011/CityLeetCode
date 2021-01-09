9717c299ac7548eeacb61f6473424ade66ac5cf3
huangcheng <huangcheng_1993@126.com>
2021-01-09 12:05:20 +0800

    2021/01/09

---
+++ leetCode/src/array/L189.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Arrays;
+
+public class L189 {
+    public static void main(String[] args) {
+        L189 m = new L189();
+        m.rotate(new int [] {-1,-100,3,99,8}, 2);
+    }
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        if(k == 0) return;
+        int [] tmp = new int [k];
+        for(int i = 0; i < k; i++){
+            tmp[i] = nums[i + n - k];
+        }
+        for(int i = n - 1; i >= k; i--){
+            System.out.println(i +"  " +(i - n + k + 1));
+            nums[i] = nums[i - k];
+        }
+        System.out.println(Arrays.toString(nums));
+        for(int i = 0; i < k; i++){
+            nums[i] = tmp[i];
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
